e2b4a3a5fca5df3f07e371ca2b94a3cd64f44ccf
Astrid Allemandou <aalleman@z1r1p5.le-101.fr>
2019-10-21 11:35:46 +0200

    Add parameter in function test to run multiple times

--- test-gnl.c
+++ test-gnl.c
@@ -6,7 +6,7 @@
 /*   By: aalleman <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/17 15:10:26 by aalleman     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/18 13:42:39 by aalleman    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/21 11:34:58 by aalleman    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,23 +17,29 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-void	test(int fd, char **line)
+void	test(int fd, char **line, int testnb)
 {
 	int		ret;
 	char	*ptr;
+	int		i;
 
 	ptr = *line;
-	ret = get_next_line(fd, line);
-	if (ret != 1)
+	i = 0;
+	while (i < testnb)
 	{
-		printf("Return %i\n", ret);
-		if (ptr != *line && *line)
-			printf("Pointer has changed : %p -> %p.\n", ptr, *line);
-	}
-	else if (ret == 1)
-	{
-		printf("Line : [%s]\n", *line);
-		free(*line);
+		ret = get_next_line(fd, line);
+		if (ret != 1)
+		{
+			printf("Return %i\n", ret);
+			if (ptr != *line && *line)
+				printf("Pointer has changed : %p -> %p.\n", ptr, *line);
+		}
+		else if (ret == 1)
+		{
+			printf("Line : [%s]\n", *line);
+			free(*line);
+		}
+		i++;
 	}
 }
 
@@ -66,10 +72,7 @@ int		main(int ac, char **av)
 	{
 		printf("\nVery long lines :\n");
 		fd = open("testfilesgnl/longlines", O_RDONLY);
-		test(fd, &line);
-		test(fd, &line);
-		test(fd, &line);
-		test(fd, &line);
+		test(fd, &line, 4);
 		return (0);
 	}
 
@@ -77,156 +80,104 @@ int		main(int ac, char **av)
 
 	line = 0;
 	printf("\nNegative fd :\n");
-	test(-2, &line);
+	test(-2, &line, 1);
 
 	printf("\nNo file :\n");
-	test(3, &line);
+	test(42, &line, 1);
 
 	printf("\nEmpty file :\n");
 	fd = open("testfilesgnl/empty", O_RDONLY);
-	test(fd, &line);
+	test(fd, &line, 1);
 
 	printf("\nOnly newline :\n");
 	fd = open("testfilesgnl/onlynewline", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 2);
 
 	printf("\nOnly a :\n");
 	fd = open("testfilesgnl/onlya", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 2);
 
 	printf("\nOnly z + nl :\n");
 	fd = open("testfilesgnl/onlyznl", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 2);
 
 	printf("\nLorem ipsum :\n");
 	fd = open("testfilesgnl/loremipsum", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 3);
 
 	printf("\nLorem ipsum + nl :\n");
 	fd = open("testfilesgnl/loremipsumnl", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 2);
 
 	printf("\nMultiple lines :\n");
 	fd = open("testfilesgnl/multiplelines", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 6);
 
 	printf("\nMultiple short lines :\n");
 	fd = open("testfilesgnl/multipleshortlines", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 6);
 
 	printf("\nMultiple lines, last line is long :\n");
 	fd = open("testfilesgnl/multiplelineslonglast", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 6);
 
 	printf("\nOnly empty lines (4) :\n");
 	fd = open("testfilesgnl/emptylines", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 5);
 
 	printf("\nSome empty lines (begin with full, finish with full) :\n");
 	fd = open("testfilesgnl/someemptylines", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 6);
 
 	printf("\nSome empty lines (begin with empty, finish with empty) :\n");
 	fd = open("testfilesgnl/someemptylines2", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 8);
 
 #if BUFFER_SIZE == 32
 
 	printf("\n31 chars :\n");
 	fd = open("testfilesgnl/31chars", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 2);
 
 	printf("\n30 chars + nl :\n");
 	fd = open("testfilesgnl/30charsplusnl", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 2);
 
 	printf("\n32 chars :\n");
 	fd = open("testfilesgnl/32chars", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 2);
 
 	printf("\n31 chars + nl :\n");
 	fd = open("testfilesgnl/31charsplusnl", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 2);
 
 	printf("\n33 chars :\n");
 	fd = open("testfilesgnl/33chars", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 2);
 
 	printf("\n32 chars + nl :\n");
 	fd = open("testfilesgnl/32charsplusnl", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line), 2;
 
 	printf("\n33 chars + nl :\n");
 	fd = open("testfilesgnl/33charsplusnl", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 2);
 
 	printf("\n30 chars + nl and small line :\n");
 	fd = open("testfilesgnl/30charsplusnlmore", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 3);
 
 	printf("\n31 chars + nl and small line :\n");
 	fd = open("testfilesgnl/31charsplusnlmore", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 3);
 
 	printf("\n32 chars + nl and small line :\n");
 	fd = open("testfilesgnl/32charsplusnlmore", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 3);
 
 	printf("\n33 chars + nl and small line :\n");
 	fd = open("testfilesgnl/33charsplusnlmore", O_RDONLY);
-	test(fd, &line);
-	test(fd, &line);
-	test(fd, &line);
+	test(fd, &line, 3);
 #endif
 
 	return (0);
